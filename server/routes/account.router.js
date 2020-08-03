72ce9fe72ead1b5cdceca1011fa527365f92b97d
Luke Johnson <lukejohnson3712@gmail.com>
2020-08-03 09:34:43 -0500

    Basic tests.

--- server/routes/account.router.js
+++ server/routes/account.router.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../modules/pool');
 
-router.get('/', (req, res) => {
+router.get('/:id', (req, res) => {
+  // TODO Hook up to a database.
+  // const exampleObj = {
+
+  // };
+  // const queryText = 'INSERT INTO "game" ("bgg_game_id", "game_img", "title", "player_range", "playtime") VALUES ($1, $2, $3, $4, $5);';
+  // pool.query(queryText, [gameID, artwork, title, playerRange, playTime])
+  //   .then(queryResponse => res.send(queryResponse))
+  //   .catch((error) => {
+  //     console.log(error);
+  //     res.sendStatus(500);
+  //   });
+  // res.status(200).send();
   res.sendStatus(200);
 });
 
--- server/tests/account.router.test.js
+++ server/tests/account.router.test.js
@@ -1,10 +1,14 @@
 const app = require('../server');
-const testServer = require('supertest');
+const testServer = require('supertest')(app);
 
 describe('Test the account path', () => {
-  it('It should respond 200 the account owner', async (done) => {
-    const response = await testServer(app).get('/api/account');
-    expect(response.statusCode).toBe(200);
+  it('It should respond with the account owner', async (done) => {
+    const res = await testServer(app).get('/api/account');
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toEqual({
+      id: 1,
+      description: "test location"
+    });
     done();
   });
 });
