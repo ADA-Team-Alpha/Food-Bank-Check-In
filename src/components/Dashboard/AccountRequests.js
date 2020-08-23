b286b843c8b9a825f0418fadfdd93c70d52c9d0f
Luke Johnson <lukejohnson3712@gmail.com>
2020-08-23 15:53:19 -0500

    Add buttons for accepting or declining an account request.

--- src/components/Dashboard/AccountRequests.js
+++ src/components/Dashboard/AccountRequests.js
@@ -4,6 +4,7 @@ import MUIDataTable from 'mui-datatables';
 import TablePresets from '../Table/TableDefaults';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core';
 import "./Dashboard.css";
+import "./AccountRequests.css";
 
 const useStyles = createMuiTheme(
   TablePresets.theme
@@ -32,16 +33,58 @@ class PendingAccounts extends Component {
     const data = this.props.pendingAccounts.map(accountObj =>
       [accountObj.name, accountObj.email, accountObj.household_id]
     );
-    console.log(data);
+    const columns = TablePresets.columns;
+    // If there's the default quantity of three columns add another one for updating accounts
+    // because every time the pages updates it will try to add another column.
+    if (columns.length === 3) {
+      columns.push(
+        {
+          name: 'Update',
+          options: {
+            filter: true,
+            sort: false,
+            empty: true,
+            customBodyRenderLite: (dataIndex) => {
+              return (
+                <>
+                  <button
+                    className={'acceptButton'}
+                    onClick={() => {
+                      this.props.dispatch({
+                        type: 'UPDATE_PENDING_ACCOUNT',
+                        payload: { id: this.props.pendingAccounts[dataIndex].id, approved: true }
+                      })
+                    }}>
+                    Accept
+                </button>
+                  <button
+                    className={'declineButton'}
+                    onClick={() => {
+                      this.props.dispatch({
+                        type: 'UPDATE_PENDING_ACCOUNT',
+                        payload: { id: this.props.pendingAccounts[dataIndex].id, approved: false }
+                      })
+                    }}>
+                    Decline
+                </button>
+                </>
+              );
+            }
+          }
+        }
+      )
+    }
     return (
       <>
+        <h5>If any of this information needs to be modified please speak with an administrator.</h5>
         <MuiThemeProvider theme={useStyles}>
-          <MUIDataTable title='Search Page' data={data} columns={TablePresets.columns} options={TablePresets.options} />
+          <MUIDataTable title='Pending Accounts' data={data} columns={TablePresets.columns} options={TablePresets.options} />
         </MuiThemeProvider>
       </>
     );
   }
 }
+
 
 const mapStateToProps = (state) => ({
   pendingAccounts: state.pendingAccounts
